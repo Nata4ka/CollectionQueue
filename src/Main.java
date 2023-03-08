74c25d3eb5847fd1ba0ae57f33c8f15f93a91ba5
Natalia Novohatskaja <natalinovohatskaja@gmail.com>
2023-03-08 12:48:05 +0300

    bugs fixed

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+
 import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Queue<Person> queue = new ArrayDeque<>(generateClients());
@@ -9,12 +11,9 @@ public class Main {
             int ticket = person.getTicket();
             if (ticket > 0) {
                 person.setTicket(ticket - 1);
-
-
-                System.out.println(person + " -прокатился(-лась) на аттракционе. Осталось " + person.getTicket() + " билет(-a,-ов)");
-                //person.setTicket(ticket-1);
+                System.out.println(person + " -прокатился(-лась) на аттракционе. " +
+                        "Осталось " + person.getTicket() + " билет(-a,-ов)");
             }
-
             if (ticket == 0) {
                 System.out.println(person + "- билеты закончились");
             } else if (ticket > 0) {
@@ -22,17 +21,13 @@ public class Main {
             }
         }
     }
-    public static List<Person> generateClients(){
+    public static List<Person> generateClients() {
         List<Person> client = new ArrayList<>();
-        client.add(new Person("Кристина","Войтова",3 ));
-        client.add(new Person("Станислав","Сергиенко",4));
-        client.add(new Person("Каролина","Максимова",2));
-        client.add(new Person("Виталий","Иванов",1));
-        client.add(new Person("Любовь","Коралёва",5));
+        client.add(new Person("Кристина", "Войтова", 3));
+        client.add(new Person("Станислав", "Сергиенко", 4));
+        client.add(new Person("Каролина", "Максимова", 2));
+        client.add(new Person("Виталий", "Иванов", 1));
+        client.add(new Person("Любовь", "Коралёва", 5));
         return client;
     }
-
-
-
-
 }
--- src/Person.java
+++ src/Person.java
@@ -1,35 +1,37 @@
 public class Person {
 
-        protected String name;
-        protected String surName;
-        protected int ticket;
-      public Person(String name,String surName,int ticket){
-          this.name=name;
-          this.surName=surName;
-          this.ticket=ticket;
-      }
+    protected String name;
+    protected String surName;
+    protected int ticket;
 
-        public String getName() {
-            return name;
-        }
-
-        public String getSurName() {
-            return surName;
-        }
-
-        public int getTicket() {
-            return ticket;
-        }
-
-        public void setTicket(int ticket) {
-            this.ticket = ticket;
-        }
-        @Override
-        public String toString(){
-          return name + " " + surName ;
-        }
-
-
+    public Person(String name, String surName, int ticket) {
+        this.name = name;
+        this.surName = surName;
+        this.ticket = ticket;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public int getTicket() {
+        return ticket;
+    }
+
+    public void setTicket(int ticket) {
+        this.ticket = ticket;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + surName;
+    }
+
+
+}
 
 
